0daae3428561aa0a6022af92e100816c9f06dfc5
Maria Alejandra Delgado Narvaez <m.delgadon@uniandes.edu.co>
2025-04-27 09:28:17 -0500

    Update EntrenamientoSpotify.py

--- model_deployment/EntrenamientoSpotify.py
+++ model_deployment/EntrenamientoSpotify.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeRegressor
 import joblib
 
 # Cargar los datos
@@ -19,15 +20,16 @@ y = data['popularity']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Definir el modelo
-clf = DecisionTreeClassifier(max_depth=5, random_state=42)
+#clf = DecisionTreeClassifier(max_depth=5, random_state=42)
+clf = DecisionTreeRegressor(max_depth=5, random_state=42)
 
 # Entrenar el modelo
 clf.fit(X_train, y_train)
 
 # Evaluar el modelo
-y_pred = clf.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy en datos de prueba: {accuracy:.2f}")
+#y_pred = clf.predict(X_test)
+#accuracy = accuracy_score(y_test, y_pred)
+#print(f"Accuracy en datos de prueba: {accuracy:.2f}")
 
 # Guardar el modelo
 joblib.dump(clf, 'ModeloEntrenado.pkl', compress=3)
